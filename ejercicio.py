0b54c15b3522725b529315f690ab1726d0a1f168
AndresCR24 <c-andres-98@hotmail.com>
2023-05-05 07:14:29 -0500

    se arregla el codigo

--- ejercicio.py
+++ ejercicio.py
@@ -6,24 +6,25 @@ class PatternMatcher:
         if not isinstance(pattern, str):
             raise ValueError("El patrón debe ser un string")
 
-        elif not all(isinstance(string, str) for string in strings):
+        elif not all(isinstance(string, str) for string in strings): 
             raise TypeError ("Solo puede contener strings")
         
+        pattern = pattern.replace('*', '.*')
         matching_strings = []
         for string in strings:
-            if re.search(pattern,string):
+            if re.match(pattern,string):
                 matching_strings.append(string)
-                #pattern = pattern.replace('*', '.*')
+                
         return matching_strings
 
 
 # Ejemplo de uso:
 
 matcher = PatternMatcher()
-strings = ["gato", "perro", "caballo", "gorila", "gaviota",2]
-strings_2 = ["carro", "casa", "cementerios", "papa", "mama", "oso"]
-pattern = "g.*a"
-pattern2 = "c.*o"
+strings = ["gato", "perro", "caballo", "gorila", "gaviota", "ardilla"]
+strings_2 = ["carro", "casa", "cementerio", "papa", "mama", "oso"]
+pattern = "g*a"
+pattern2 = "c*o"
 matching_strings = matcher.match(strings, pattern)
 matching_strings_2 = matcher.match(strings_2, pattern2)
 
